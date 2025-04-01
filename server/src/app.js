ad05d374d32dbb79228cf0f415956263af4d3a3c
Alfred <alfred-brannarer@hotmail.com>
2025-04-01 08:51:08 +0200

    Updated from cors to vite server proxy

--- server/src/app.js
+++ server/src/app.js
@@ -1,16 +1,9 @@
 import express from 'express';
-import cors from 'cors';
-import apiRoutes from './routes/routes.js'
 import createRoutes from './routes/routes.js';
 
 function initApp(api) {
     const app = express();
 
-    const corsOptions = {
-        origin: ['http://localhost:5173'],
-    };
-
-    app.use(cors(corsOptions));
     app.use(express.json());
     app.use('/api', createRoutes(api));
 
