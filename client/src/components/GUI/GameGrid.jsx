a3639a81241b4a01f3b822df6aaa0665ec62fce7
Alfred <alfred-brannarer@hotmail.com>
2025-04-02 13:42:59 +0200

    Added logic for the game grid

--- client/src/components/GUI/GameGrid.jsx
+++ client/src/components/GUI/GameGrid.jsx
@@ -1,13 +1,37 @@
-export default function GameGrid({ guesses }) {
+export default function GameGrid({ guesses, wordLength }) {
+    let emptyRows = 5 - guesses.length;
+    let totalRows = [];
+
+    for (let rows = 0; rows < 5; rows++) {
+        let row = [];
+
+        const currentGuess = guesses[rows];
+
+        for (let col = 0; col < wordLength; col++) {
+            if (currentGuess) {
+                const letterObj = currentGuess[col];
+                row.push(
+                    <span key={col} className={`letter-box ${letterObj.result}`}>
+                        {letterObj.letter}
+                    </span>
+                );
+            } else {
+                row.push(
+                    <span key={col} className="letter-box empty"></span>
+                );
+            }
+        }
+
+        totalRows.push(
+            <div key={`guess-${rows}`} className="row">
+                {row}
+            </div>
+        );
+    }
+
     return (
         <div>
-            {guesses.map((guess, guessIndex) => (
-                <div key={guessIndex} className="row">
-                    {guess.map((letterObj, letterIndex) => (
-                        <span key={letterIndex} className={`letter-box ${letterObj.result}`}>{letterObj.letter} </span>
-                    ))}
-                </div>
-            ))}
+            {totalRows}
         </div>
     );
 }
