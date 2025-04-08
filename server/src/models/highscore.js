54dbbc7c31a24762f15d527c1f321492b971d919
Alfred <alfred-brannarer@hotmail.com>
2025-04-08 13:58:51 +0200

    Add correctWord to DB for future filtering

--- server/src/models/highscore.js
+++ server/src/models/highscore.js
@@ -3,7 +3,8 @@ import mongoose from 'mongoose';
 const highScoreSchema = new mongoose.Schema({
     name: { type: String, required: true },
     guesses: { type: Number, required: true },
-    time: { type: Number, required: true }
+    time: { type: Number, required: true },
+    word: { type: String, required: true }
 });
 
 export default mongoose.model('HighScore', highScoreSchema, 'Highscores');
